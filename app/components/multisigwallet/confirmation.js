afa5ce4fd56352dc517fc4c0279fbd2a90d89c64
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2019-04-11 23:11:20 -0300

    more icons

--- app/components/multisigwallet/confirmation.js
+++ app/components/multisigwallet/confirmation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import TransactionSubmitButton from '../transaction-submit-button';
+import { ButtonGroup } from 'react-bootstrap';
 class MSWConfirmation extends React.Component {
     constructor(props) {
         super(props);
@@ -83,7 +84,7 @@ class MSWConfirmation extends React.Component {
         const { accountConfirmed, confirmations, required } = this.state;
         const { MultiSigWallet, id, account} = this.props;
         return (
-            <React.Fragment>
+            <ButtonGroup aria-label="Transaction Actions">
                 { (confirmations >= required) &&
                 <TransactionSubmitButton 
                     account={account}
@@ -93,6 +94,11 @@ class MSWConfirmation extends React.Component {
                     onSubmission={(txHash) => this.handleSubmission(txHash) }
                     onResult={(result) => this.handleResult(result) }
                     onError={(error) => this.handleError(error) }
+                    icon={
+                        <svg className="svg-icon" viewBox="0 0 20 20">
+							<path d="M17.218,2.268L2.477,8.388C2.13,8.535,2.164,9.05,2.542,9.134L9.33,10.67l1.535,6.787c0.083,0.377,0.602,0.415,0.745,0.065l6.123-14.74C17.866,2.46,17.539,2.134,17.218,2.268 M3.92,8.641l11.772-4.89L9.535,9.909L3.92,8.641z M11.358,16.078l-1.268-5.613l6.157-6.157L11.358,16.078z"></path>
+						</svg>
+                    }
                     text="Execute"
                     variant={accountConfirmed ? "danger" : "success"}
                     />
@@ -125,7 +131,7 @@ class MSWConfirmation extends React.Component {
                     }
                     variant={accountConfirmed ? "danger" : "success"}
                     />
-            </React.Fragment>
+            </ButtonGroup>
             
         )
     }
--- app/components/multisigwallet/deployer.js
+++ app/components/multisigwallet/deployer.js
@@ -75,7 +75,7 @@ class MSWDeployer extends React.Component {
         return(
             <Card>
                 <Card.Header>
-                    Deploy New MultiSigWallet
+                    Deploy MultiSigWallet
                 </Card.Header>
                 <ListGroup variant="flush">
                     <ListGroup.Item>
@@ -89,7 +89,7 @@ class MSWDeployer extends React.Component {
                         </div>
                     </ListGroup.Item>
                 </ListGroup>
-                <Card.Body>
+                <Card.Body className="text-right">
                     <TransactionSubmitButton 
                         account={account}
                         sendTransaction={
@@ -98,6 +98,11 @@ class MSWDeployer extends React.Component {
                         onSubmission={(txHash) => this.handleSubmission(txHash) }
                         onResult={(result) => this.handleResult(result) }
                         onError={(error) => this.handleError(error) }
+                        icon={
+                            <svg className="svg-icon" viewBox="0 0 20 20">
+							    <path d="M15.475,6.692l-4.084-4.083C11.32,2.538,11.223,2.5,11.125,2.5h-6c-0.413,0-0.75,0.337-0.75,0.75v13.5c0,0.412,0.337,0.75,0.75,0.75h9.75c0.412,0,0.75-0.338,0.75-0.75V6.94C15.609,6.839,15.554,6.771,15.475,6.692 M11.5,3.779l2.843,2.846H11.5V3.779z M14.875,16.75h-9.75V3.25h5.625V7c0,0.206,0.168,0.375,0.375,0.375h3.75V16.75z"></path>
+						    </svg>
+                        }
                         text="Deploy"
                         size="sm"
                         />
