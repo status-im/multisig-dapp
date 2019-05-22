74df33e596b9e5b1ae31e54567411412c2435c6b
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2019-05-22 04:07:38 -0300

    better responsiveness for ethaddress

--- app/components/EthAddress.jsx
+++ app/components/EthAddress.jsx
@@ -4,6 +4,7 @@ import Blockies from 'react-blockies';
 import { Overlay, Tooltip } from 'react-bootstrap';
 import copy from 'copy-to-clipboard';
 import './EthAddress.css';
+import ClipIcon from "./Icon/Clip";
 
 const nullAddress = "0x0000000000000000000000000000000000000000"
 class EthAddress extends React.Component {
@@ -22,6 +23,7 @@ class EthAddress extends React.Component {
 
 	static defaultProps = {
 		className: 'eth-address',
+		value: null,
 		defaultValue: nullAddress,
 		colors: true,
 		control: false,
@@ -38,47 +40,53 @@ class EthAddress extends React.Component {
 		this.controlRef = React.createRef();
 		this.attachRef = containerRef => this.setState({ containerRef });
 		this.state = {
-			address: props.address != undefined ? props.address : props.defaultValue,
+			value: null,
+			address: nullAddress,
+			ensReverse: null,
+			valid: false,
 			tooltipVisible: false 
 		};
 	}
 
 
 	componentDidMount() {
-
+		this.setValue(this.props.value || this.props.defaultValue);
     }
 
 	componentDidUpdate(prevProps, prevState) {
         if (prevProps.value != this.props.value && this.props.value != this.state.value) {
-            this.setState({value: this.props.value});
+            this.setValue(this.props.value);
 		}
 		if(prevState.value != this.state.value) {
-			this.checkValue(this.state.value);	
+			this.checkValue();	
 		}
 		if(prevState.address != this.state.address) {
-			this.checkAddress(this.state.address);
-			this.props.onChange(this.state.address);
+			this.checkAddress();
 		}
     }
 
-	checkAddress(address) {
+	setValue(value){
+		if(this.props.control) {
+			this.controlRef.current.textContent = value;
+		}
+		this.setState({value, address: nullAddress, ensReverse: null});
+	}
+
+	checkAddress() {
+		const { value, address } = this.state;
 		if(address != nullAddress){
 			EmbarkJS.Names.lookup(address, (err, name) => {
-				if(err){
-					console.log("lookup ERR ", address, err, name);
-				} else {
-					console.log("the domain of "+address+" is: " + name);
-				}
 				this.setState({ensReverse: name});
+				this.props.onChange(address, value, name);
 			})
 		} else {
 			this.setState({ensReverse: null});
+			this.props.onChange(address, value, null);
 		}
-
-	}
-
-	checkValue(value) {
-		console.log("value is" + value)
+	}	
+	
+	checkValue() {
+		const { value } = this.state;
 		if(value == null){
 			this.setState({address: nullAddress, valid: this.props.allowZero});
 		}else if(value.startsWith("0x")) {
@@ -91,10 +99,8 @@ class EthAddress extends React.Component {
 		} else {
 			EmbarkJS.Names.resolve(value, (err, result) => {
 				if(err){
-					console.log('ENS err', value, err, result)
 					this.setState({address: nullAddress, valid: false});
 				} else {
-					console.log("ENS address of "+value+" is " + result)
 					const valid = !err && result != nullAddress;
 					this.setState({address: result, valid});	
 				}
@@ -173,9 +179,7 @@ class EthAddress extends React.Component {
 						/>
 					: 
 					<span className="clip-icon">
-						<svg xmlns='http://www.w3.org/2000/svg' viewBox="0 0 20 20" width="15" height="15">
-							<path d="M4.317,16.411c-1.423-1.423-1.423-3.737,0-5.16l8.075-7.984c0.994-0.996,2.613-0.996,3.611,0.001C17,4.264,17,5.884,16.004,6.88l-8.075,7.984c-0.568,0.568-1.493,0.569-2.063-0.001c-0.569-0.569-0.569-1.495,0-2.064L9.93,8.828c0.145-0.141,0.376-0.139,0.517,0.005c0.141,0.144,0.139,0.375-0.006,0.516l-4.062,3.968c-0.282,0.282-0.282,0.745,0.003,1.03c0.285,0.284,0.747,0.284,1.032,0l8.074-7.985c0.711-0.71,0.711-1.868-0.002-2.579c-0.711-0.712-1.867-0.712-2.58,0l-8.074,7.984c-1.137,1.137-1.137,2.988,0.001,4.127c1.14,1.14,2.989,1.14,4.129,0l6.989-6.896c0.143-0.142,0.375-0.14,0.516,0.003c0.143,0.143,0.141,0.374-0.002,0.516l-6.988,6.895C8.054,17.836,5.743,17.836,4.317,16.411"></path>
-						</svg>
+						<ClipIcon width={15} fill="#CCC"/>
 					</span> 
 					}
 					<Overlay target={containerRef} show={tooltipVisible} placement="bottom">
