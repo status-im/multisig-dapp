521b1f0da909349f1f58a9a0f39d8337303c4e29
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2019-05-22 09:55:58 -0300

    register embark.eth at dev

--- config/namesystem.js
+++ config/namesystem.js
@@ -5,6 +5,12 @@ module.exports = {
     provider: "ens"
   },
   development: {
+    register: {
+      rootDomain: "eth",
+      subdomains: {
+        'embark': '0x1a2f3b98e434c02363f3dac3174af93c1d690914'
+      }
+    }
   },
   testnet: {
   },
