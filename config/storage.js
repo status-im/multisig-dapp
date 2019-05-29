84c5d08c9ddff2d9dfedcc47cb25f7b5f6f5cb52
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2019-05-29 00:14:49 -0300

    add swarm-gateways to dappConnection

--- config/storage.js
+++ config/storage.js
@@ -14,6 +14,12 @@ module.exports = {
         host: "localhost",
         port: 8500,
         getUrl: "http://localhost:8500/bzzr:/"
+      },
+      {
+        provider:"swarm",
+        host: "swarm-gateways.net",
+        port: 80,
+        getUrl: "http://swarm-gateways.net/bzzr:/"
       }
     ]
   },
