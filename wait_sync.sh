ee434c22f33d84c8b9f1bb7ab3ee330315b48468
Ricardo Guilherme Schmidt <ricardo3@status.im>
2020-04-11 13:42:54 -0300

    fix synced check

--- wait_sync.sh
+++ wait_sync.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 SYNCED=0;
-echo "Waiting for light sync"; 
+echo "Waiting for peers..."; 
 while [ $SYNCED -eq 0 ] ; 
 do 
     tail -n 3 blockchain.log; 
     sleep 10; 
-    SYNCED=`geth attach /tmp/embark-*/geth.ipc --exec "eth.blockNumber != 0 && eth.syncing === false ? 1 : 0"`; 
+    SYNCED=`geth attach /tmp/embark-*/geth.ipc --exec "net.peerCount != 0 && eth.blockNumber != 0 ? 1 : 0"`; 
+done
+echo "Light sync started..."; 
+sleep 60
+SYNCED=0;
+while [ $SYNCED -eq 0 ] ; 
+do 
+    tail -n 3 blockchain.log; 
+    sleep 10; 
+    SYNCED=`geth attach /tmp/embark-*/geth.ipc --exec "net.peerCount != 0 && eth.syncing === false ? 1 : 0"`; 
 done
